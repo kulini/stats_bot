6117fe23f7b5f1d2e6ab98bcb395007b0e872463
hlee <hlee@trilogyed.com>
2018-11-20 11:22:14 -0500

    Restores slack client

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import re
 import csv
 from slackclient import SlackClient
 
+# instantiate Slack client
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+# starterbot's user ID in Slack: value is assigned after the bot starts up
+starterbot_id = None
+
 # constants
 RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
 QUESTION_COMMAND = "quiz me"
